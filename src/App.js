9b743b16057e197aaac080ca0ecf5f744d915950
AndresRamirezMunoz <andes028@hotmail.com>
2020-10-08 01:02:19 -0500

    commit correcion infraccion interfaz

--- src/App.js
+++ src/App.js
@@ -3,13 +3,84 @@ import logo from './logo.svg';
 import './App.css';
 import Infraccion from './components/infracciones'
 
-function App() {
-  return (
-    <div className="App">
-      <Infraccion/>
-      
-    </div>
-  );
+export default class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      infracciones: false,
+      agente: false,
+      conductor: false,
+      vehiculo: false
+    };
+    this.items = [
+      {
+        label: 'Infraccion',
+        icon: 'pi pi-fw pi-file',
+        command: () => { this.showInfracciones() }
+      },
+      {
+        label: 'Agente',
+        icon: 'pi pi-fw pi-user',
+        command: () => { this.showAgente() }
+      },
+      {
+        label: 'Conductor',
+        icon: 'pi pi-fw pi-user',
+        command: () => { this.showConductor() }
+      },
+      {
+        label: 'Vehiculo',
+        icon: 'pi pi-fw pi-align-center',
+        command: () => { this.showVehiculo() }
+      }
+    ]
+  }
+
+  componentDidMount() {
+  }
+
+  render() {
+    return (
+      <div>
+        <Menubar model={this.items} />
+        <br />
+        <Infraccion />
+      </div>
+    );
+  }
+  showInfracciones() {
+    this.setState({
+      infracciones: true,
+      agente: false,
+      conductor: false,
+      vehiculo: false
+    })
+  }
+  showAgente() {
+    this.setState({
+      infracciones: false,
+      agente: true,
+      conductor: false,
+      vehiculo: false
+    })
+  }
+  showConductor() {
+    this.setState({
+      infracciones: false,
+      agente: false,
+      conductor: true,
+      vehiculo: false
+    })
+  }
+  showVehiculo() {
+    this.setState({
+      infracciones: false,
+      agente: false,
+      conductor: false,
+      vehiculo: true
+    })
+  }
+
 }
 
 export default App;
